2852aa69062e98d5c07f6a3b09667f86464a1506
Saher Ismail <112554103+RehasLiamsi@users.noreply.github.com>
2023-10-20 10:33:00 +0200

    About us update (#38)
    * Update aboutUs
    
    * Update image

--- AgilProjekt/src/components/AboutPage.vue
+++ AgilProjekt/src/components/AboutPage.vue
@@ -5,11 +5,23 @@
 
 <template>
   <div class="about-section">
+  <div class="top">
     <h1>About Us</h1>
-    <h2>Just Go</h2>
+    <img src="../assets/Untitled.jpg">
+    </div>
+    <h2>Welcome to Quiztastic</h2>
     <p class="description">
-      A squad of four diligent students has crafted a quiz that's as entertaining as it is enlightening. Get ready for a good time with some seriously witty questions!
-    </p>
+      We are a passionate team of four dedicated individuals who share a common goal: to make learning engaging, fun, and accessible to everyone. 
+      As students ourselves, we understand the challenges and joys of the educational journey, which inspired us to create this innovative quiz app 
+      for our school project.    </p>
+      <h3>Meet Our Team</h3>
+      <ul class="team">
+      <li>Stephen Osedumme</li>
+      <li>Saher Ismail</li>
+      <li>Venu Modi</li>
+      <li>Wassim Tajeddin</li>
+      </ul>
+    
   </div>
 </template>
 
@@ -17,13 +29,26 @@
 
 
  .about-section {
+  display: flex;
   text-align: center;
-  padding: 20px;
+  flex-direction: column;
+  padding: 20px 0;
 }
 
+.top{
+  display: flex;
+  justify-content: space-evenly;
+    align-items: center;
+    padding: 4rem;
+}
   h1 {
     color: #0f9d58;
     font-size: 5em;
+  }
+
+  img{
+    width: 40em;
+    height: auto;
   }
 
   h2 {
@@ -31,9 +56,28 @@
     font-size: 3em;
   }
 
+  h3{
+    color:#0f9d58;
+    font-size: 2em;
+  }
+
   .description {
     font-size: 1.2em;
     color: white;
-    margin: 100px;
+    margin: 20px 40px 100px 40px;
+    padding: 0 100px;
+  
+  }
+
+  .team{
+    list-style: none;
+    padding-left: 0;
+    color: white;
+    margin-top: 0;
+    font-size: 1.2em;
+  }
+
+  .team li{
+    margin:0;
   }
 </style>
