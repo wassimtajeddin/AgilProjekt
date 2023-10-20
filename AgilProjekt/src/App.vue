2fb46a3cf5ea55e3753b403c93f917578afdfd6c
Saher Ismail <112554103+RehasLiamsi@users.noreply.github.com>
2023-10-20 10:31:33 +0200

    Categories dropdown menu (#37)
    * Delete div class homeview
    
    * Add dropdown menu for categories and change hover

--- AgilProjekt/src/App.vue
+++ AgilProjekt/src/App.vue
@@ -1,61 +1,73 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue'
+
+const showDropdown = ref(false)
+
+const toggleDropdown = () => {
+  showDropdown.value = !showDropdown.value
+}
+
+window.addEventListener('click', (event) => {
+  if (!event.target.closest('.category-dropdown')) {
+    showDropdown.value = false
+  }
+})
 </script>
 
 <template>
-    <header>
+  <header>
     <nav>
       <RouterLink to="/">Home</RouterLink>
     </nav>
     <nav>
       <RouterLink to="/about">About Us</RouterLink>
     </nav>
-      <img alt="game logo" class="logo" src="@/assets/Quiztastic.png" width="150" height="150" />
-      <nav>
+    <img alt="game logo" class="logo" src="@/assets/Quiztastic.png" width="150" height="150" />
+    <nav>
       <RouterLink to="/login">Login</RouterLink>
     </nav>
+    <div class="category-dropdown" @click="toggleDropdown">
       <nav>
-      <RouterLink to="/categories">Categories</RouterLink>
+        <span class="category-link">Categories</span>
       </nav>
-    </header>
-    <div class="homeview">
-      <RouterView />
+      <div v-if="showDropdown" class="dropdown-content">
+        <RouterLink to="/category/1">History</RouterLink>
+        <RouterLink to="/category/2">Mathematics</RouterLink>
+        <!-- Add more categories as needed -->
+      </div>
     </div>
-
-  
+  </header>
+  <RouterView />
 </template>
 
 <style scoped>
-
 header {
-
-  background-color:#005B41;
+  background-color: #005b41;
   padding: 20px 200px;
   display: flex;
   justify-content: space-between;
   align-items: center;
   flex-wrap: wrap;
-  margin-bottom: 50px;
-}
-
-nav:hover {
-    transform: rotate(5deg);
-    transition: all 0.3s ease-out;
 }
 
 nav {
-    display: block;
-    padding: 1rem;
-    font-size: 1.5rem;
-}
-a:hover {
-  color: black;
+  display: flex;
+  align-items: center;
+  font-size: 2.2rem;
+  margin-bottom: 50px;
 }
 
-a{
+a,
+.category-link {
   text-decoration: none;
   color: white;
-  font-family: math; 
+  cursor: pointer;
+}
+
+.category-dropdown {
+  position: relative;
+  display: inline-block;
 }
 
 img{
@@ -63,13 +75,43 @@ img{
   height: 80px;
 }
 
-/*.homeview {
-  font-family: Arial, Helvetica, sans-serif;
-  background-color: #e2f3ec;
 
-}*/
+.dropdown-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  background-color: white;
+  border-bottom-left-radius: 2rem;
+  border-bottom-right-radius: 2rem;
+  min-width: 160px;
+  left: 50%;
+  transform: translateX(-50%);
+}
 
+.dropdown-content a {
+  color: black;
+}
 
+a,
+.category-link {
+  padding: 0.5rem;
+  text-decoration: none;
+  color: white;
+}
 
+a::after,
+.category-link::after {
+  content: '';
+  display: block;
+  width: 0;
+  height: 2px;
+  background: #000;
+  transition: width 0.3s;
+}
 
+a:hover::after,
+.category-link:hover::after {
+  width: 100%;
+}
 </style>
