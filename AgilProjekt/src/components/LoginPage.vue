027ea529db85ddabf66fb3cad52e15961ec2f7ec
Osedumme Steven <stephen.osedumme@iths.se>
2023-10-23 02:33:32 +0200

    first commit

--- AgilProjekt/src/components/LoginPage.vue
+++ AgilProjekt/src/components/LoginPage.vue
@@ -1,59 +1,7 @@
-<script setup>
-import { Form, Field } from 'vee-validate';
-import * as Yup from 'yup';
-
-/*import { useAuthStore } from '@/stores';*/
-
-const schema = Yup.object().shape({
-    username: Yup.string().required('Username is required'),
-    password: Yup.string().required('Password is required')
-});
-
-/*async function onSubmit(values) {
-    const authStore = useAuthStore();
-    const { username, password } = values;
-    await authStore.login(username, password);
-}
-/*import { ref, computed } from 'vue'
-
-const player = ref({
-  name: '',
-  email:'',
-  password:''
-})
-
-// a computed ref
-// eslint-disable-next-line vue/return-in-computed-property
-const logIn = computed(() => {
-  console.log("logedIn", player)
-})*/
-</script>
+<script setup></script>
 
 <template>
-<div class="card-m3">
-  <h1 class="card-header">Login</h1>
-  <div class="card-body">
-      <Form @submit="onSubmit" :validation-schema="schema" v-slot="{ errors, isSubmitting }">
-          <div class="form-group">
-              <label>Username</label>
-              <Field name="username" type="text" class="form-control" :class="{ 'is-invalid': errors.username }" />
-              <div class="invalid-feedback">{{ errors.username }}</div>
-          </div>
-          <div class="form-group">
-              <label>Password</label>
-              <Field name="password" type="password" class="form-control" :class="{ 'is-invalid': errors.password }" />
-              <div class="invalid-feedback">{{ errors.password }}</div>
-          </div>
-          <div class="form-group">
-              <button class="btn btn-primary" :disabled="isSubmitting">
-                  <span v-show="isSubmitting" class="spinner-border spinner-border-sm mr-1"></span>
-                  Login
-              </button>
-              <router-link to="register" class="btn btn-link">Register</router-link>
-          </div>
-      </Form>
-  </div>
-</div>
+  <div></div>
 </template>
   
 <style>
